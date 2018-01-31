043f6b35e1f7ae7ecbd5126cd11833d2a9fbea52
Tuomas <tuomas.nurminen@hotmail.com>
2018-01-31 15:12:56 +0200

    First Additions
    Added Models folder
    Added Book model
    Added WebControllers folder
    Added BookController Controller
    Added a Project notes file called TehtavanAnto

---
+++ Bookstore/src/main/java/fi/haagahelia/spring/Bookstore/WebControllers/BookController.java
@@ -0,0 +1,14 @@
+package fi.haagahelia.spring.Bookstore.WebControllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BookController {
+	
+	@RequestMapping(value="/index", method=RequestMethod.GET)
+	public String index(){
+		return "index";
+	}
+}
